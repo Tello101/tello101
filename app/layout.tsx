68a05c646b3ad6a2d67be40116704b6ff3bc4b73
sum-in-e <suminkim.me@gmail.com>
2025-04-16 23:29:07 +0900

    fix: 모바일 이슈

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en' suppressHydrationWarning>
-			<body className='min-h-screen bg-background flex flex-col'>{children}</body>
+			<head>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+			</head>
+			<body className='min-h-screen bg-background flex flex-col overflow-x-hidden'>{children}</body>
 		</html>
 	);
 }
