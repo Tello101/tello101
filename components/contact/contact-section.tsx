86770d013e71e5c69bab067d306f1c70f787c7c4
sum-in-e <suminkim.me@gmail.com>
2025-04-21 17:27:37 +0900

    fix: contact us

--- components/contact/contact-section.tsx
+++ components/contact/contact-section.tsx
@@ -5,12 +5,12 @@ import { ContactForm } from '@/components/contact/contact-form';
 export const ContactSection = () => {
 	return (
 		<section className='lg:p-5'>
-			<div className='flex flex-col lg:flex-row lg:rounded-xl lg:overflow-hidden bg-gray-100'>
+			<div className='flex flex-col lg:flex-row lg:rounded-xl lg:overflow-hidden'>
 				<div className='w-full lg:w-1/3 bg-primary'>
 					<ContactInfo />
 				</div>
 
-				<div className='w-full lg:w-2/3 p-6 lg:p-10'>
+				<div className='w-full lg:w-2/3 p-6 lg:p-10 '>
 					<ContactForm />
 				</div>
 			</div>
